35f570affd44b137cbd3748cd37867562474143a
MyBeee <90430226+MyBeee@users.noreply.github.com>
2024-09-28 00:20:49 +0900

    Add files via upload

--- JavaProgramming2/src/Week_03_02/Hometask05.java
+++ JavaProgramming2/src/Week_03_02/Hometask05.java
@@ -3,37 +3,53 @@ package Week_03_02;
 import java.util.Scanner;
 
 public class Hometask05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.print("지원자 수: ");
-		int a = scanner.nextInt();
-		int[][] scores = new int[a][4];
-		int sum = 0;
-		double avg = 0;
-		int score = 0;
-		int max = 0;
-		int min = 1000;
-		
-		for(int i = 0; i<scores.length; i++) {
-			sum = 0;
-			for(int j = 0; j<scores[i].length; j++) {
-				System.out.printf("%d지원자의 점수를 입력: ", (i+1));
-				score = scanner.nextInt();
-				scores[i][j] = score;
-				sum += scores[i][j];
-			}
-			avg = (double)sum / 4;
-			System.out.printf("지원자 %d의 총합: %d\n", i+1 ,sum);
-			System.out.printf("지원자 %d의 평균: %d\n", i+1 ,avg);
-			if(max < sum) {
-				max = sum;
-			}
-			if(min > sum) {
-				min = sum;
-			}
-		}
-	}
+        System.out.print("지원자 수: ");
+        int numberOfApplicants = scanner.nextInt();
 
+        int[][] scores = new int[numberOfApplicants][4];
+        
+        double maxAvg = 0, minAvg = 100;
+        int max = 0, min = 1000;
+        int maxIndex = -1, minIndex = -1;
+
+        for (int i = 0; i < numberOfApplicants; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < scores[i].length; j++) {
+                String scoreType = "";
+                switch (j) {
+                    case 0: scoreType = "인성테스트"; break;
+                    case 1: scoreType = "코딩테스트"; break;
+                    case 2: scoreType = "필기"; break;
+                    case 3: scoreType = "실기"; break;
+                }
+
+                System.out.printf("%d지원자의 %s 점수를 입력: ", (i + 1), scoreType);
+                scores[i][j] = scanner.nextInt();
+                sum += scores[i][j];
+            }
+
+            double avg = (double) sum / scores[i].length;
+            System.out.printf("지원자 %d의 총합: %d\n", i + 1, sum);
+            System.out.printf("지원자 %d의 평균: %.2f\n", i + 1, avg);
+
+            if (sum > max) {
+                max = sum;
+                maxAvg = avg;
+                maxIndex = i;
+            }
+            if (sum < min) {
+                min = sum;
+                minAvg = avg;
+                minIndex = i;
+            }
+        }
+
+        // 최고 점수 및 최소 점수 지원자 출력
+        System.out.printf("최고 점수를 받은 지원자: %d지원자 (총합: %d)(평균: %.2f)\n", maxIndex + 1, max, maxAvg);
+        System.out.printf("최소 점수를 받은 지원자: %d지원자 (총합: %d)(평균: %.2f)\n", minIndex + 1, min, minAvg);
+    }
 }
