c1a8645e9b4b7f01417fb28353c419d4b4005f2b
MyBeee <90430226+MyBeee@users.noreply.github.com>
2024-10-08 09:09:21 +0900

    Add files via upload

---
+++ JavaProgramming2/src/Week_06_01/Task01.java
@@ -0,0 +1,13 @@
+package Week_06_01;
+
+public class Task01 {
+
+	public static void main(String[] args) {
+		Car myCar = new Car("포르쉐");
+		Car youCar = new Car("벤츠");
+		
+		myCar.run();
+		youCar.run();
+	}
+
+}
---
+++ JavaProgramming2/src/Week_06_01/Task03.java
@@ -0,0 +1,9 @@
+package Week_06_01;
+
+public class Task03 {
+
+	public static void main(String[] args) {
+		System.out.println(Television.info);
+	}
+
+}
